27212226a04bec9f325a374130fce1ae54beeffa
jakubiecdaniel <daniel.jakubiec@stonybrook.edu>
2022-04-10 22:40:58 -0400

    catch webdriver exception

--- src/Wikipedia.py
+++ src/Wikipedia.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 import selenium
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException 
+from selenium.webdriver.chrome.options import Options
 
 def GetFeaturedArticleURL():
     baseURL = "https://en.wikipedia.org"
@@ -34,11 +36,94 @@ def GetFeaturedArticleURL():
 
     return FeaturedArticleURL
 
+#https://stackoverflow.com/questions/45199076/take-full-page-screenshot-in-chrome-with-selenium   
+def chrome_takeFullScreenshot(driver) :
+  import json
+  import base64
+  def send(cmd, params):
+    resource = "/session/%s/chromium/send_command_and_get_result" % driver.session_id
+    url = driver.command_executor._url + resource
+    body = json.dumps({'cmd':cmd, 'params': params})
+    response = driver.command_executor._request('POST', url, body)
+    return response.get('value')
+
+  def evaluate(script):
+    response = send('Runtime.evaluate', {'returnByValue': True, 'expression': script})
+    return response['result']['value']
+
+  metrics = evaluate( \
+    "({" + \
+      "width: Math.max(window.innerWidth, document.body.scrollWidth, document.documentElement.scrollWidth)|0," + \
+      "height: Math.max(innerHeight, document.body.scrollHeight, document.documentElement.scrollHeight)|0," + \
+      "deviceScaleFactor: window.devicePixelRatio || 1," + \
+      "mobile: typeof window.orientation !== 'undefined'" + \
+    "})")
+  send('Emulation.setDeviceMetricsOverride', metrics)
+  screenshot = send('Page.captureScreenshot', {'format': 'png', 'fromSurface': True})
+  send('Emulation.clearDeviceMetricsOverride', {})
+
+  return base64.b64decode(screenshot['data'])
+
+def headless_pic(URL):
 
 
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--log-level=1')
+    #options.add_argument('--window-size=1300,1000')
+
+    #Todo, add check that chromedriver and chrome are installed
+    try:
+        driver = webdriver.Chrome(executable_path='C:\\chromedriver\\chromedriver.exe',chrome_options=options)
+    except WebDriverException:
+        print("Chromedriver needs to be installed.")
+        return False
+    
+    print(driver.get_window_size())
+    driver.get(URL)
+    
+    try:
+        notes_el = driver.find_element_by_id("Notes")
+        notes_el.click()
+    except NoSuchElementException:
+        pass
+    try:
+        ref_el = driver.find_element_by_id("References")
+        ref_el.click()
+    except NoSuchElementException:
+        pass
+    try:
+        extlinks_el = driver.find_element_by_id("External_links")
+        extlinks_el.click()
+    except NoSuchElementException:
+        pass
+    
+    scrollHeight = driver.execute_script("return document.body.scrollHeight")
+    print(scrollHeight)
+    inc = (scrollHeight / 10.0)
+    scroll = 0
+    for i in range(0,10):
+        scrollTo = inc * (i + 1)
+        ScrollScript = "window.scrollTo(0," + str(scrollTo) + ")"
+        driver.execute_script(ScrollScript)
+        time.sleep(2)
+    driver.execute_script("window.scrollTo(0,0)")
+    
+    png = chrome_takeFullScreenshot(driver)
+
+    with open("sc.png", 'wb') as f:
+        f.write(png)
+    
+    driver.close()
+    return 'sc.png'
 
 def TakePicture(URL): 
-    browser = webdriver.Firefox()
+    try:
+        browser = webdriver.Firefox()
+    except WebDriverException:
+        print("Geckodriver needs to be installed.")
+        return False
     try:
         browser.get(URL)
         browser.set_context("chrome")
